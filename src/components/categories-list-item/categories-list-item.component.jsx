56ccb6ebb0cb22b0fbd6058624b019012ccfea8d
Melkfree <yevhen.ihnatiuk@gmail.com>
2022-05-13 20:36:33 +0300

    add routes to home component

--- src/components/categories-list-item/categories-list-item.component.jsx
+++ src/components/categories-list-item/categories-list-item.component.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import { useNavigate } from 'react-router-dom';
 import { BackgroundImage, Body, CategoriesListItemContainer} from './categories-list-item.styles';
 
 const CategoriesListItem = ({ category }) => {
-    const {imageUrl, title} = category;
+    const {imageUrl, title, route } = category;
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route);
     return(
-        <CategoriesListItemContainer >
+        <CategoriesListItemContainer onClick={onNavigateHandler}>
           <BackgroundImage imageUrl={imageUrl} />
           <Body>
             <h2>{title}</h2>
