90f55450f5191dfb9b44cd1123195d24e174a08e
Melkfree <yevhen.ihnatiuk@gmail.com>
2022-05-23 15:46:14 +0300

    deleting categories context after migration on redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 import { store } from './store/store';
 
@@ -15,11 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>        
+        <CartProvider>
+          <App />
+        </CartProvider> 
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
