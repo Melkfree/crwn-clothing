c140619eda39490542db8bd8ec613e8faffff6be
Melkfree <yevhen.ihnatiuk@gmail.com>
2022-07-11 19:37:37 +0300

    migration on redux-saga

--- src/store/store.js
+++ src/store/store.js
@@ -6,20 +6,29 @@ import { rootReducer } from './root-reducer';
 
 import { loggerMiddleware } from './middleware/logger';
 
+// import thunk from 'redux-thunk';
+import createSagaMiddleware from '@redux-saga/core';
+
+import { rootSaga } from './root-saga';
+
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user']
+    whitelist: ['cart']
 }
+
+const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && loggerMiddleware].filter(Boolean);
+const middleWares = [process.env.NODE_ENV !== 'production' && loggerMiddleware, sagaMiddleware].filter(Boolean);
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window  && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const composedEnchancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnchancers);
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
